5196973f93fe93ebad51a3f4818291f706d01745
arthur <arthur.tavaresquintao@student.kuleuven.be>
2023-04-21 17:44:53 +0200

    add email bundl

--- book_binder/config/bundles.php
+++ book_binder/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
