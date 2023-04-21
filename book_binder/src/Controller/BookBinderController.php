b6c42d379b6de036863d97da14e3848230c6bc24
arthur <arthur.tavaresquintao@student.kuleuven.be>
2023-04-21 12:54:24 +0200

    add simple controller for index.html.twig

---
+++ book_binder/src/Controller/BookBinderController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class BookBinderController extends AbstractController
+{
+    /**
+     * @Route("/", name="book_binder")
+     */
+    public function index(): Response
+    {
+        return $this->render('book_binder/index.html.twig', [
+            'controller_name' => 'BookBinderController',
+        ]);
+    }
+}
