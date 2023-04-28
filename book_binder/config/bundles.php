73122b6821bad97f02f67994ddcf89846d07f8fd
arthur <arthur.tavaresquintao@student.kuleuven.be>
2023-04-28 21:02:26 +0200

    add webpack encore

--- book_binder/config/bundles.php
+++ book_binder/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
