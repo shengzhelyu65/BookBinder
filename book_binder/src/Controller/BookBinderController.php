66471e9243a7bd719ca7668d2010d18f23384b0a
arthur <arthur.tavaresquintao@student.kuleuven.be>
2023-04-28 16:17:00 +0200

    remove use

--- book_binder/src/Controller/BookBinderController.php
+++ book_binder/src/Controller/BookBinderController.php
@@ -6,11 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Annotation\Security;
 
 class BookBinderController extends AbstractController
 {
-
     #[Route('/', name: 'index')]
     public function index(): Response
     {
