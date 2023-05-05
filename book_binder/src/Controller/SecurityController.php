f9828d9e0bb82afe2e798943453ca53b38bca243
Shengzhe Lyu <mishengzhelv@outlook.com>
2023-05-05 16:48:58 +0200

    persist user into db with google authentication

--- book_binder/src/Controller/SecurityController.php
+++ book_binder/src/Controller/SecurityController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -28,5 +32,37 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/login/google', name: 'app_login_google')]
+    public function googleCheckAction(Request $request, EntityManagerInterface $entityManager): RedirectResponse
+    {
+        // Get the credential from the request
+        $credential = $request->request->get('credential');
+
+        // Split the credential into three parts: header, payload, and signature
+        $parts = explode('.', $credential);
+
+        // Decode the payload using Base64url decoding
+        $payload = json_decode(base64_decode($parts[1]), true);
+
+        // Get the email from the payload
+        $email = $payload['email'];
+
+        // check if the user already exists in the database
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+        if (!$user) {
+            // if the user doesn't exist, create a new user object and set its properties
+            $user = new User();
+            $user->setEmail($email);
+            $user->setPassword('');
+
+            // persist the user object to the database
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        // redirect the user to a new page
+        return new RedirectResponse($this->generateUrl('app_login'));
     }
 }
