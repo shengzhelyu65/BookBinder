3e1ff991ebc13ff8763c8991aca36174ca4a05d3
Maarten <mmedaer@gmail.com>
2023-05-05 14:38:49 +0200

    Added ID decoding for retrieving user information through google sign in

--- book_binder/src/Controller/BookBinderController.php
+++ book_binder/src/Controller/BookBinderController.php
@@ -22,9 +22,23 @@ class BookBinderController extends AbstractController
     {
         $credential = $request->request->get('credential');
 
+        // Split the credential into three parts: header, payload, and signature
+        $parts = explode('.', $credential);
+
+        // Decode the payload using Base64url decoding
+        $payload = json_decode(base64_decode($parts[1]), true);
+
+        // Get the email from the payload
+        $email = $payload['email'];
+        $given_name = $payload['given_name'];
+        $family_name = $payload['family_name'];
+
         return $this->render('book_binder/index.html.twig', [
             'controller_name' => 'BookBinderController',
             'credential' => $credential,
+            'email' => $email,
+            'given_name' => $given_name,
+            'family_name' => $family_name
         ]);
     }
 }
