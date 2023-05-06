fc16086705ee1e8c4ce83f3e56b61557f179b5c8
Shengzhe Lyu <mishengzhelv@outlook.com>
2023-05-06 19:21:09 +0200

    fix the authentication of security bundle with google

--- book_binder/src/Controller/SecurityController.php
+++ book_binder/src/Controller/SecurityController.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -16,9 +18,9 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+//        if ($this->getUser()) {
+//            return $this->redirectToRoute('app_home');
+//        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -35,7 +37,7 @@ class SecurityController extends AbstractController
     }
 
     #[Route(path: '/login/google', name: 'app_login_google')]
-    public function googleCheckAction(Request $request, EntityManagerInterface $entityManager): RedirectResponse
+    public function googleCheckAction(Request $request, EntityManagerInterface $entityManager, TokenStorageInterface $tokenStorage): RedirectResponse
     {
         // Get the credential from the request
         $credential = $request->request->get('credential');
@@ -62,7 +64,22 @@ class SecurityController extends AbstractController
             $entityManager->flush();
         }
 
-        // redirect the user to a new page
-        return new RedirectResponse($this->generateUrl('app_login'));
+        // authenticate the user
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+        if (!$user) {
+            // create a new user
+            $user = new User();
+            $user->setEmail($email);
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        // authenticate the user
+        $token = new UsernamePasswordToken($user, 'main', $user->getRoles());
+
+        // store the token in the token storage
+        $tokenStorage->setToken($token);
+
+        return new RedirectResponse($this->generateUrl('app_home'));
     }
 }
