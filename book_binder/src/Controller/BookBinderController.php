8b9db899d7371457a5adb4b1c97894b8641e2415
arthur <arthur.tavaresquintao@student.kuleuven.be>
2023-05-07 21:30:31 +0200

    add profile info form

--- book_binder/src/Controller/BookBinderController.php
+++ book_binder/src/Controller/BookBinderController.php
@@ -6,14 +6,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Security;
 
 class BookBinderController extends AbstractController
 {
+
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(Security $security, EntityManagerInterface $entityManager): Response
     {
+        $includeProfileForm = false; // Set this to true or false depending on your condition
+
+        $this->security = $security;
+        $user = $this->security->getUser();
+
+        // print_r($user);
+        // Get current user entity object from the database using repository method by email
+        $email = $user->getEmail();
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+        // print_r($user->getUserIdentifier());
+
+        // Check if the userPersonalInfo entity object is null
+        if ($user->getUserPersonalInfo() == null) {
+            // If it is null, set the includeProfileForm to false
+            $includeProfileForm = true;
+        }
+
         return $this->render('book_binder/index.html.twig', [
             'controller_name' => 'BookBinderController',
+            'includeProfileForm' => $includeProfileForm,
+            'userEmail' => $email,
         ]);
     }
 
