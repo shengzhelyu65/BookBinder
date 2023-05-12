e8871a196d3764d65912529e82f2361b598d3311
Shengzhe Lyu <mishengzhelv@outlook.com>
2023-05-12 13:30:37 +0200

    add basis for searching

--- book_binder/src/Controller/BookReviewsController.php
+++ book_binder/src/Controller/BookReviewsController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Form\BookReviewFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\BookReviews;
@@ -44,6 +45,35 @@ class BookReviewsController extends AbstractController
 
         return $this->render('book_reviews/book_review_list.html.twig', [
             'bookReviews' => $bookReviews,
+        ]);
+    }
+
+    #[Route('/book/reviews/search', name: 'book_reviews_search')]
+    public function searchReviews(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $query = $request->query->get('q');
+
+        // Search for reviews that contain the query string in the review text or tags
+        $bookReviews = $entityManager->getRepository(BookReviews::class)->createQueryBuilder('r')
+            ->where('r.review LIKE :query OR r.tags LIKE :query')
+            ->setParameter('query', '%' . $query . '%')
+            ->getQuery()
+            ->getResult();
+
+        // Transform the reviews into an array of data that can be easily encoded as JSON
+        $reviewsData = [];
+        foreach ($bookReviews as $review) {
+            $reviewsData[] = [
+                'review_ID' => $review->getReviewID(),
+                'rating' => $review->getRating(),
+                'review' => $review->getReview(),
+                'tags' => $review->getTags(),
+            ];
+        }
+
+        return $this->render('book_reviews/book_review_search.html.twig', [
+            'bookReviews' => $bookReviews,
+            'query' => $query,
         ]);
     }
 }
