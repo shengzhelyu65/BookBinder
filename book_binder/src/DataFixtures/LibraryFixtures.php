0884ed86c207e7d3acdf37438f5cd5ef4f8e94ee
Shengzhe Lyu <mishengzhelv@outlook.com>
2023-05-19 15:13:31 +0200

    add LibraryFixtures.php

--- book_binder/src/DataFixtures/LibraryFixtures.php
+++ book_binder/src/DataFixtures/LibraryFixtures.php
@@ -15,7 +15,7 @@ class LibraryFixtures extends Fixture
         $library->setZipCode(1000);
         $library->setCity('Brussels');
         $library->setHouseNumber(115);
-
+        $library->setStreet('Waterloolaan');
         $library->setNumber('+32 2 542 65 11');
         $library->setWebsite('https://justitie.belgium.be/nl/over_de_fod_diensten/bibliotheek');
         $library->setEmail('bibliotheek@just.fgov.be');
@@ -28,6 +28,7 @@ class LibraryFixtures extends Fixture
         $library->setZipCode(2060);
         $library->setCity('Antwerp');
         $library->setHouseNumber(25);
+        $library->setStreet('De Coninckplein');
         $library->setNumber('+32 3 338 38 00');
         $library->setWebsite('https://www.permeke.org/');
         $library->setEmail('permeke@antwerpen.be');
@@ -40,9 +41,23 @@ class LibraryFixtures extends Fixture
         $library->setZipCode(2000);
         $library->setCity('Antwerp');
         $library->setHouseNumber(1);
+        $library->setStreet('Prinsstraat');
         $library->setNumber('+32 3 265 21 11');
+        $library->setWebsite('https://www.uantwerpen.be/nl/bibliotheek/');
+        $library->setEmail('libraryUniversity@antwerpen.be');
 
-        
+        $manager->persist($library);
+        $manager->flush();
+
+        $library = new Library();
+        $library->setLibraryName('KU Leuven Libraries');
+        $library->setZipCode(3000);
+        $library->setCity('Leuven');
+        $library->setHouseNumber(1);
+        $library->setStreet('Prinsstraat');
+        $library->setNumber('+32 16 32 46 00');
+        $library->setWebsite('https://bib.kuleuven.be/english');
+        $library->setEmail('bib@kuleuven.be');
     }
 }
 
