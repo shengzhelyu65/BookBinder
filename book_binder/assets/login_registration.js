63cbd1c4d3c65e85485493c81d75fa1ab42191a5
arthur <arthur.tavaresquintao@student.kuleuven.be>
2023-05-19 15:07:59 +0200

    fix javascript depedecy issue on twig template

--- book_binder/assets/login_registration.js
+++ book_binder/assets/login_registration.js
@@ -13,4 +13,16 @@ import './bootstrap';
 
 require('bootstrap');
 
+import $ from 'jquery';
+import 'select2';
 
+
+$(document).ready(function () {
+    $('.multiple-select-field').select2({
+        theme: "bootstrap-5",
+        width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
+        placeholder: $(this).data('placeholder'),
+        closeOnSelect: false,
+        maximumSelectionLength: 5,
+    });
+});
