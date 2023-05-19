a974c560a14ad6f0aeb27a5467c75295410c7429
arthur <arthur.tavaresquintao@student.kuleuven.be>
2023-05-19 17:30:34 +0200

    remove unused code

--- book_binder/src/Controller/BookBinderController.php
+++ book_binder/src/Controller/BookBinderController.php
@@ -11,34 +11,7 @@ use Symfony\Component\Security\Core\Security;
 
 class BookBinderController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(Security $security, EntityManagerInterface $entityManager): Response
-    {
-        $includeProfileForm = false; // Set this to true or false depending on your condition
-
-        $this->security = $security;
-        $user = $this->security->getUser();
-
-        // print_r($user);
-        // Get current user entity object from the database using repository method by email
-        $email = $user->getEmail();
-        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
-        // print_r($user->getUserIdentifier());
-
-        // Check if the userPersonalInfo entity object is null
-        if ($user->getUserPersonalInfo() == null) {
-            // If it is null, set the includeProfileForm to false
-            $includeProfileForm = true;
-        }
-
-        return $this->render('book_binder/index.html.twig', [
-            'controller_name' => 'BookBinderController',
-            'includeProfileForm' => $includeProfileForm,
-            'userEmail' => $email,
-        ]);
-    }
-
-    #[Route("/home", name: 'app_home')]
+    #[Route("/", name: 'app_home')]
     public function home(): Response
     {
         return $this->render('book_binder/index.html.twig', [
