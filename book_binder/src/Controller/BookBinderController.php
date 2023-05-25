0987a89aa161249807a55f4b56174ce74a4f4242
arthur <arthur.tavaresquintao@student.kuleuven.be>
2023-05-25 20:28:42 +0200

    update dependencies and fix routes mismatch

--- book_binder/src/Controller/BookBinderController.php
+++ book_binder/src/Controller/BookBinderController.php
@@ -13,8 +13,10 @@ use App\Api\GoogleBooksApiClient;
 
 class BookBinderController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(Security $security, EntityManagerInterface $entityManager): Response
+    #[Route("/index", name: 'app_home')]
+    #[Route("/home", name: 'app_home')]
+    #[Route("/", name: 'app_home')]
+    public function home(EntityManagerInterface $entityManager): Response
     {
         // ============= API stuff =============
         $ApiClient = new GoogleBooksApiClient();
@@ -37,37 +39,10 @@ class BookBinderController extends AbstractController
 
         // =============
 
-        $includeProfileForm = false; // Set this to true or false depending on your condition
-
-        $this->security = $security;
-        $user = $this->security->getUser();
-
-        // print_r($user);
-        // Get current user entity object from the database using repository method by email
-        $email = $user->getEmail();
-        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
-        // print_r($user->getUserIdentifier());
-
-        // Check if the userPersonalInfo entity object is null
-        if ($user->getUserPersonalInfo() == null) {
-            // If it is null, set the includeProfileForm to false
-            $includeProfileForm = true;
-        }
-
         return $this->render('book_binder/index.html.twig', [
             'controller_name' => 'BookBinderController',
-            'includeProfileForm' => $includeProfileForm,
-            'userEmail' => $email,
             'results' => $results,
             'reviews' => $reviews
-        ]);
-    }
-
-    #[Route("/home", name: 'app_home')]
-    public function home(): Response
-    {
-        return $this->render('book_binder/index.html.twig', [
-            'controller_name' => 'BookBinderController',
         ]);
     }
 }
