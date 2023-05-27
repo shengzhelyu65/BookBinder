4bc85a34459fe83cbdf7fb834795e5ba8b31a9f3
arthur <arthur.tavaresquintao@student.kuleuven.be>
2023-05-27 13:03:58 +0200

    draft for ReadingListController

--- book_binder/src/Controller/ReadingListController.php
+++ book_binder/src/Controller/ReadingListController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Entity\UserPersonalInfo;
+use App\Entity\UserReadingInterest;
+use App\Entity\UserReadingList;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +18,24 @@ class ReadingListController extends AbstractController
     #[Route('/reading-list', name: 'reading_list')]
     public function showReadingList(EntityManagerInterface $entityManager): Response
     {
-        // creeate empty JSON arrays to hold the results
-        $currently_reading = [];
-        $want_to_read = [];
-        $have_read = [];
+
+        // fetch the user's reading list
+        $user = $this->getUser();
+        $user_reading_list = $user->getUserReadingList();
+
+        // if the user has a reading list, fetch the books
+        if ($user_reading_list) {
+            $currently_reading = $user_reading_list->getCurrentlyReading();
+            $want_to_read = $user_reading_list->getWantToRead();
+            $have_read = $user_reading_list->getHaveRead();
+        }
+
+        // query the Books table based on the ids in currently_reading
+
+        // query the Books table based on the ids in want_to_read
+
+        // query the Books table based on the ids in have_read
+
 
         // render the reading list page
         return $this->render('reading_list/reading_list.html.twig', [
