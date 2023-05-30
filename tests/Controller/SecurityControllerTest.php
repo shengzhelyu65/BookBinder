d3b8e15a3e2ae55cdf56930654f58bb76033c5c8
Shengzhe Lyu <mishengzhelv@outlook.com>
2023-05-30 20:40:40 +0200

    add login controller

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Entity\User;
+use Symfony\Component\Panther\PantherTestCase;
+
+class SecurityControllerTest extends PantherTestCase
+{
+    public function testLogin(): void
+    {
+        $client = static::createPantherClient();
+
+        // Simulate a GET request to the /logout page
+        $crawler = $client->request('GET', '/logout');
+
+        // Assert that the user is redirected to the home page
+        $this->assertStringContainsString('/login', $client->getCurrentURL());
+
+        $this->assertStringContainsString('Sign in', $client->getPageSource());
+
+        // Fill in the login form
+        $form = $crawler->filter('form.form-signin')->form();
+        $form['email'] = 'test@test.com';
+        $form['password'] = 'password123';
+
+        // Submit the form
+        $crawler = $client->submit($form);
+
+        // Assert that the reading interest form submission was successful and redirected to the home page
+        $this->assertStringContainsString('/', $client->getCurrentURL());
+
+        // Assert that the home page is displayed
+        $this->assertSelectorTextContains('h3', 'mystery');
+    }
+}
