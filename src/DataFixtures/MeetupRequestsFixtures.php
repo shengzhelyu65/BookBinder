8a08219baa4e8198cd975f34607c175326cd8d2f
Shengzhe Lyu <mishengzhelv@outlook.com>
2023-06-01 23:42:06 +0200

    add MeetupRequestsFixtures

---
+++ src/DataFixtures/MeetupRequestsFixtures.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Library;
+use App\Entity\MeetupRequestList;
+use App\Entity\MeetupRequests;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class MeetupRequestsFixtures extends Fixture implements DependentFixtureInterface
+{
+    public const MEETUP_REQUEST_REFERENCE = 'meetup_request_ref';
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        for ($i = 1; $i <= 10; $i++) {
+            $meetupRequest = new MeetupRequests();
+            $library = $this->getReference(LibraryFixtures::LIBRARY_REFERENCE . $faker->numberBetween(1, 5));
+            $meetupRequest->setLibraryID($library);
+            $maxNumber = $faker->numberBetween(5, 10);
+
+            // Create users array with size maxNumber
+            for ($k = 1; $k <= $maxNumber; $k++) {
+                $users[] = $this->getReference(UserFixtures::USER_REFERENCE . $faker->numberBetween(1, 10));
+            }
+            $meetupRequest->setMaxNumber($maxNumber);
+            $meetupRequest->setHostUser($users[0]);
+            $book = $this->getReference(BookFixtures::BOOK_REFERENCE . $faker->numberBetween(1, 10));
+            $meetupRequest->setBookID($book->getGoogleBooksId());
+            $meetupRequest->setDatetime($faker->dateTimeBetween('now', '+1 month'));
+
+            $manager->persist($meetupRequest);
+
+            // Create unique reference for each meetup request
+            $this->addReference(self::MEETUP_REQUEST_REFERENCE . $i, $meetupRequest);
+
+            // Create meetup request list
+            $requestsNumber = $faker->numberBetween(1, $maxNumber);
+            for ($j = 1; $j <= $requestsNumber; $j++) {
+                $meetupRequestList = new MeetupRequestList();
+                $meetupRequestList->setUserID($users[$j]);
+                $meetupRequestList->setMeetupID($meetupRequest);
+                $manager->persist($meetupRequestList);
+            }
+
+            // Create meetup list
+            $meetupNumber = $faker->numberBetween(0, $maxNumber - $requestsNumber);
+            for ($j = 1; $j <= $meetupNumber; $j++) {
+                $meetupList = new MeetupRequestList();
+                $meetupRequestList->setUserID($users[$j+$requestsNumber]);
+                $meetupRequestList->setMeetupID($meetupRequest);
+                $manager->persist($meetupRequestList);
+            }
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            LibraryFixtures::class,
+            UserFixtures::class,
+            BookFixtures::class,
+        ];
+    }
+}
