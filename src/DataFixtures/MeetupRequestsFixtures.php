36a91cd9ef53ade708aff37400556f19675df17e
Shengzhe Lyu <mishengzhelv@outlook.com>
2023-06-02 02:28:22 +0200

    fix the bug of MeetupRequestsFixtures

--- src/DataFixtures/MeetupRequestsFixtures.php
+++ src/DataFixtures/MeetupRequestsFixtures.php
@@ -17,6 +17,7 @@ class MeetupRequestsFixtures extends Fixture implements DependentFixtureInterfac
         $faker = Factory::create();
 
         $users = [];
+        $users_shuffled = [];
 
         for ($i = 1; $i <= 10; $i++) {
             $meetupRequest = new MeetupRequests();
@@ -25,10 +26,18 @@ class MeetupRequestsFixtures extends Fixture implements DependentFixtureInterfac
             $maxNumber = $faker->numberBetween(5, 10);
 
             // Create users array with size maxNumber
-            for ($k = 1; $k <= $maxNumber; $k++) {
-                $users[$k-1] = $this->getReference(UserFixtures::USER_REFERENCE . $k);
+            $users[0] = $this->getReference(UserFixtures::USER_REFERENCE . $i);
+            for ($k = 1; count($users_shuffled) <= $maxNumber - 1; $k++) {
+                // Skip the host user
+                if ($k == $i) {
+                    continue;
+                }
+                $users_shuffled[$k] = $this->getReference(UserFixtures::USER_REFERENCE . $k);
             }
-            shuffle($users);
+            shuffle($users_shuffled);
+            for ($j = 1; $j <= $maxNumber - 1; $j++) {
+                $users[$j] = $users_shuffled[$j];
+            }
 
             $meetupRequest->setMaxNumber($maxNumber);
             $meetupRequest->setHostUser($users[0]);
