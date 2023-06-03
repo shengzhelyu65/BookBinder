d2e66a0b5c3e0612ec88d7285c19be4ec4a49a26
Shengzhe Lyu <mishengzhelv@outlook.com>
2023-06-03 18:09:24 +0200

    add user entity tests

--- tests/Repository/UserRepositoryTest.php
+++ tests/Repository/UserRepositoryTest.php
@@ -3,6 +3,9 @@
 namespace App\Tests\Repository;
 
 use App\Entity\User;
+use App\Entity\UserPersonalInfo;
+use App\Entity\UserReadingInterest;
+use App\Entity\UserReadingList;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class UserRepositoryTest extends KernelTestCase
@@ -52,6 +55,56 @@ class UserRepositoryTest extends KernelTestCase
 
         $user = $this->userRepository->findOneBy(['email' => 'test@example.com']);
         $this->assertNull($user);
+    }
+
+    public function testEraseCredentials()
+    {
+        $this->assertNotNull($this->userRepository->findOneBy(['id' => 1])->getPassword());
+    }
+
+    public function testEntitySetInfo()
+    {
+        $user = new User();
+        $user->setEmail('test@example.com');
+        $user->setPassword('password123');
+        $user->setRoles(['ROLE_USER']);
+        $this->userRepository->save($user, true);
+
+        $user1 = $this->userRepository->findOneBy(['email' => 'user1@example.com']);
+
+        // test user personal info
+        $personalInfo = new UserPersonalInfo();
+        $personalInfo->setUser($user1);
+        $personalInfo->setName('John');
+        $personalInfo->setSurname('Doe');
+        $personalInfo->setNickname('Johny');
+
+        $user->setUserPersonalInfo($personalInfo);
+        $this->assertNotNull($user->getUserPersonalInfo());
+        $this->assertSame($user->getUserPersonalInfo()->getUser(), $user);
+
+        // test user reading list
+        $readingList = new UserReadingList();
+        $readingList->setUser($user1);
+        $readingList->setCurrentlyReading([1, 2, 3]);
+        $readingList->setWantToRead([4, 5, 6]);
+        $readingList->setHaveRead([7, 8, 9]);
+
+        $user->setUserReadingList($readingList);
+        $this->assertNotNull($user->getUserReadingList());
+        $this->assertSame($user->getUserReadingList()->getUser(), $user);
+
+        // test user reading interest
+        $readingInterest = new UserReadingInterest();
+        $readingInterest->setUser($user1);
+        $readingInterest->setGenres(['Fantasy']);
+        $readingInterest->setLanguages(['English']);
+
+        $user->setUserReadingInterest($readingInterest);
+        $this->assertNotNull($user->getUserReadingInterest());
+        $this->assertSame($user->getUserReadingInterest()->getUser(), $user);
+
+        $this->userRepository->remove($user, true);
     }
 
     protected function tearDown(): void
