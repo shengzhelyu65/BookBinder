137f489541f69c5d772b19bdb60e61c1e0284a4f
Shengzhe Lyu <mishengzhelv@outlook.com>
2023-06-10 22:16:35 +0200

    change reject test

--- tests/Controller/MeetupRequestsControllerTest.php
+++ tests/Controller/MeetupRequestsControllerTest.php
@@ -229,7 +229,7 @@ class MeetupRequestsControllerTest extends PantherTestCase
         $this->assertTrue($response->isRedirect('/meetup/overview'));
 
         // Check if the meetup request is deleted
-        $meetupRequestList = $meetupRequestListRepository->findOneBy(['meetup_ID' => $meetup, 'user_ID' => $meetupRequestUser]);
+        $meetupRequestList = $meetupRequestListRepository->findOneBy(['meetup_request_list_ID' => $meetupRequestId]);
         $this->assertNull($meetupRequestList);
 
         // Rollback the changes
